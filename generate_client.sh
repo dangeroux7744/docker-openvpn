8712b73f3bfa226dcb1726e5dcc5701d2ae0d13f
dangeroux7744 <80545908+dangeroux7744@users.noreply.github.com>
2021-08-03 23:46:29 -0700

    Update generate_client.sh

--- generate_client.sh
+++ generate_client.sh
@@ -1,2 +1,3 @@
 export OVPN_DATA="ovpn-data-example"
 docker run -v $OVPN_DATA:/etc/openvpn --rm -it dangeroux7744/docker-openvpn easyrsa build-client-full $1 nopass
+docker run -v $OVPN_DATA:/etc/openvpn --rm dangeroux7744/docker-openvpn ovpn_getclient CLIENTNAME > CLIENTNAME.ovpn
