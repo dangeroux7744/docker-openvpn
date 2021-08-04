1408725a40a0203522058cb33eb190ce266b1964
dangeroux7744 <80545908+dangeroux7744@users.noreply.github.com>
2021-08-03 23:44:39 -0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
-#!/usr/bine/env bash
+#!/usr/bin/env bash
 export OVPN_DATA="ovpn-data-example"
 docker volume create --name $OVPN_DATA
-docker run -v $OVPN_DATA:/etc/openvpn --rm kylemanna/openvpn ovpn_genconfig -u udp://VPN.SERVERNAME.COM
-docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn ovpn_initpki
+docker run -v $OVPN_DATA:/etc/openvpn --rm dangeroux7744/docker-openvpn ovpn_genconfig -u udp://$1
+docker run -v $OVPN_DATA:/etc/openvpn --rm -it dangeroux7744/docker-openvpn ovpn_initpki
+
+docker run -v $OVPN_DATA:/etc/openvpn -d -p 1194:1194/udp --cap-add=NET_ADMIN dangeroux7744/docker-openvpn
